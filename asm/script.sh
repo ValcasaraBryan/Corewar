710b8be2396fc7193ffbcc8b80e490a5f9045d21
Bryan Valcasara <bryanvalcasara@MacBook-Air-de-Bryan.local>
2019-07-06 02:15:01 +0200

    modification message error

--- asm/script.sh
+++ asm/script.sh
@@ -16,19 +16,15 @@ do
     echo "" >> result
     echo "done $f"
 done
-echo "done result"
-echo "" > result_2
-rm $dir$cor
 for f in $dir.s
 do
-    echo $f >> result_2
-    echo "moi :" >> result_2
-    ./asm $f 2>> result_2 >> result_2
-    echo "zaz :" >> result_2
-    ./../ressources/asm $f 2>> result_2 >> result_2
-    echo "" >> result_2
-    echo "" >> result_2
+    echo $f >> result
+    echo "moi :" >> result
+    ./asm $f 2>> result >> result
+    echo "zaz :" >> result
+    ./../ressources/asm $f 2>> result >> result
+    echo "" >> result
+    echo "" >> result
     echo "done $f"
 done
-echo "done result_2"
 echo "done all"
