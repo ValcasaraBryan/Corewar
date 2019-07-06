cd80aef2d84a06c9814409b3e6ee43644379e920
Jeremy DURAND-GALLIAND <jdurand-@e1r9p15.42.fr>
2019-07-06 13:18:24 +0200

    add champion

---
+++ vm/Pogchampion.s
@@ -0,0 +1,65 @@
+.name "Pogchampion"
+.comment "Pog"
+
+live %0
+ld %57673217, r6
+fork %21
+fork %511
+
+st r1, 6
+live %0
+lfork %1856
+xor r1, r1, r2
+zjmp %-13
+
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
+st r6, 256
